d3d86bd5b225ff1e0d0703c7278b2ed872343150
Almir <contact.almir@gmail.com>
2019-10-18 19:43:14 -0500

    Create a connection to database

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql');
 
+require('dotenv').config()
+
 const app = express();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+});
+
+connection.connect(error => {
+    if (error) {
+        return error;
+    }
+});
+
+// console.log(connection);
 
 app.use(cors());
 
@@ -14,6 +31,6 @@ app.get('/products', (req, res) => {
 
 })
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Server listening on port 4000`);
 })
