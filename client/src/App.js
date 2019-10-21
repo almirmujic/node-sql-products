541682c09e9daa58f162beb5eebcd70cfd008363
Almir <contact.almir@gmail.com>
2019-10-20 22:10:14 -0500

    Input fields to create new products

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 
 class App extends Component {
   state = {
-    products: []
+    products: [],
+    product: {
+      name: '',
+      price: 0,
+    }
   }
 
   componentDidMount() {
@@ -17,14 +21,24 @@ class App extends Component {
       .catch(err => console.error(err))
   }
 
+  logProduct = e => {
+    e.preventDefault();
+    console.log(this.state.product);
+  }
+
   render() {
-    const { products } = this.state
+    const { products, product } = this.state
     return (
       <div className="App" >
         <header className="App-header">
           <ul>
             {products.map(product => <li key={product.product_id}>{product.item} is ${product.price}</li>)}
           </ul>
+          <form>
+            <input type="text" value={product.name} onChange={e => this.setState({ product: { ...product, name: e.target.value } })} />
+            <input type="text" value={product.price} onChange={e => this.setState({ product: { ...product, price: e.target.value } })} />
+            <button onClick={this.logProduct}>Submit</button>
+          </form>
         </header>
       </div>
     );
