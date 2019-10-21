17ab432c8feeefc5da6964b6db900ef89679b3d7
Almir <contact.almir@gmail.com>
2019-10-20 19:54:16 -0500

    Fetch products from database and display info client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
+  fetchProducts = () => {
+    fetch('http://localhost:4000/products')
+      .then(response => response.json())
+      .then(response => this.setState({ products: response.data }))
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    const { products } = this.state
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <ul>
+            {products.map(product => <li key={product.product_id}>{product.item} is ${product.price}</li>)}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
